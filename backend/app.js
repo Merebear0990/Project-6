ae9a92de2ff4eb77ec17a852fdbdb8ddeeb2acbb
MeredithBrandon <meredithbran2754@yahoo.com>
2022-11-26 11:24:16 -0500

    uncommented sauces api

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,24 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-const sauceRoutes = require('./routes/sauces');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-
+const userSchema = require('./models/user');
 const app = express();
 
-const connectionString = `mongodb+srv://merebear0990:<password>@cluster0.g33pgsf.mongodb.net/?retryWrites=true&w=majority
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g33pgsf.mongodb.net/?retryWrites=true&w=majority
 `
 mongoose.connect(connectionString)
-    .then(() => {
-      console.log('Successfully connectedto MongoDB Atlas!');
-    })
-    .catch((error) => {
-      console.log('Unable to connectto MongoDB Atlas!');
-      console.error(error);
-    });
+  .then(() => {
+    console.log('Successfully connectedto MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connectto MongoDB Atlas!');
+    console.error(error);
+  });
 
 app.use(express.json());
 app.use(cors());
