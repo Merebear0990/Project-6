0c527397b7d181db01f36256157b69754fa88e2f
MeredithBrandon <meredithbran2754@yahoo.com>
2022-11-26 11:13:44 -0500

    user models added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const UniqueValidator = require('mongoose-uniqure-validator'); //ensures users cannot share the same email 
+const uniqueValidator = require('mongoose-unique-validator'); //ensures users cannot share the same email 
 
 const userSchema = mongoose.Schema({
-    email: { type: String, require: true, unique: true},
-    password: { type: String, require: true, unique: true}
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true, unique: true }
 })
 
-userSchema.plugin(UniqueValidator);
+userSchema.plugin(uniqueValidator);
 
-mongoose.model.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
