aa73e6e0a8fb2a95da0b14b1a3b546caa01f5764
MeredithBrandon <meredithbran2754@yahoo.com>
2022-12-05 20:17:30 -0500

    Changes made to token split

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split('')[1];
-        const decodedToken = jwt.verify(token, 'RANDON_TOKEN_SECRET');
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = { userId };
         if (req.body.userId && req.body.userId !== userId) {
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
             next();
         }
 
-    } catch {
+    } catch (err) {
         res.status(401).json({
             error: new Error('Invalid request!')
         });
